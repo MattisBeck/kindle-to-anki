8129c4db679992f57f573deefbdae8084a9a70a8
Mattis <mattis@2a03-6880-117e-6700-3861-aa57-4d85-547e.dsl6.goetel.de>
2026-04-23 11:32:03 +0200

    Add cache managment

--- src/kindle_to_anki/db_reader.py
+++ src/kindle_to_anki/db_reader.py
@@ -1,4 +1,6 @@
+import json
 import sqlite3
+from pathlib import Path
 from dataclasses import dataclass
 
 
@@ -17,3 +19,30 @@ class WordRecord:
 class SourceBook:
     title: str
     author: str
+
+def extract_information(connection: sqlite3.Connection, cache_location: Path) -> list[WordRecord]:
+    cache = get_cache_set(cache_location)
+
+    cursor = connection.cursor()
+    cursor.execute("""
+        SELECT WORDS.word, WORDS.stem, WORDS.lang, LOOKUPS.usage, BOOK_INFO.authors, BOOK_INFO.title
+        FROM LOOKUPS 
+        JOIN WORDS ON LOOKUPS.word_key = WORDS.id
+        JOIN BOOK_INFO ON LOOKUPS.book_key = BOOK_INFO.id;;
+    """)
+
+    write_set_to_cache(cache, cache_location)
+
+def get_cache_set(cache_location: Path) -> set:
+    parent_directory = cache_location.parent
+    # Create Folder & File, if it does not exist
+    parent_directory.mkdir(parents=True, exist_ok=True)
+    if not cache_location.is_file():
+        cache_location.write_text("[]")
+    with cache_location.open("r", encoding="utf-8") as file:
+        cache = json.load(file)
+    return set(cache)
+
+def write_set_to_cache(cache_set: set, cache_location: Path) -> None:
+    with cache_location.open("w", encoding="utf-8") as file:
+        json.dump(list(cache_set), file, indent=4)
--- tests/test_db_reader.py
+++ tests/test_db_reader.py
@@ -1,10 +1,14 @@
+from sqlite3 import Connection
+from typing import Any, Generator
+
 import pytest
 import sqlite3
+from pathlib import Path
 
-from kindle_to_anki.db_reader import *
+from kindle_to_anki.db_reader import extract_information, get_cache_set, write_set_to_cache
 
 @pytest.fixture
-def db():
+def db() -> Generator[Connection, Any, None]:
     connection = sqlite3.connect(":memory:")
     with open("data/mock_vocab.sql", "r") as f:
         connection.executescript(f.read())
@@ -12,3 +16,28 @@ def db():
     yield connection
 
     connection.close()
+
+@pytest.fixture
+def cache() -> Generator[Path, Any, None]:
+    cache_location = Path("data/cache.json")
+    cache_location.write_text("[]")
+
+    yield cache_location
+
+    cache_location.unlink()
+
+
+def test_get_cache_set(cache: Path) -> None:
+    test_data = '["Hello", "World"]'
+    cache.write_text(test_data)
+    assert get_cache_set(cache) == {"Hello", "World"}
+
+def test_cache_set_create_new_file(cache: Path) -> None:
+    #Delete cache
+    cache.unlink()
+    assert get_cache_set(cache) == set()
+
+def test_write_set_to_cache(cache: Path) -> None:
+    test_data = {"Hello", "World"}
+    write_set_to_cache(test_data, cache)
+    assert get_cache_set(cache) == test_data
