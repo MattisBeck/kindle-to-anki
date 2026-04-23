cbbc01ae0f0980b97099f36f7bc5af6a349a3e98
Mattis <136255616+MattisBeck@users.noreply.github.com>
2026-04-23 21:18:34 +0200

    Add build-system

--- tests/test_db_reader.py
+++ tests/test_db_reader.py
@@ -1,16 +1,18 @@
+from multiprocessing import connection
 from sqlite3 import Connection
 from typing import Any, Generator
-
 import pytest
 import sqlite3
 from pathlib import Path
 
 from kindle_to_anki.db_reader import extract_information, get_cache_set, write_set_to_cache
+from kindle_to_anki.db_reader import WordRecord, SourceBook
 
 @pytest.fixture
 def db() -> Generator[Connection, Any, None]:
+    sql_location = Path(__file__).parent / "data" / "mock_vocab.sql"
     connection = sqlite3.connect(":memory:")
-    with open("data/mock_vocab.sql", "r") as f:
+    with sql_location.open() as f:
         connection.executescript(f.read())
 
     yield connection
@@ -41,3 +43,21 @@ def test_write_set_to_cache(cache: Path) -> None:
     test_data = {"Hello", "World"}
     write_set_to_cache(test_data, cache)
     assert get_cache_set(cache) == test_data
+
+def test_extract_information(db: sqlite3.Connection, cache: Path) -> None:
+    word_list = extract_information(db, cache)
+    # check for correct length
+    assert len(word_list) == 10
+    # check for correct type
+    assert all(isinstance(word, WordRecord) for word in word_list)
+
+    # Check attributes of first word
+
+    first_word = word_list[0]
+    assert first_word.word == "Bug"
+    assert first_word.stem == "Bug"
+    assert first_word.lang == "de"
+
+    # Test no duplicates, all should already be in cache
+    word_list = extract_information(db, cache)
+    assert len(word_list) == 0
